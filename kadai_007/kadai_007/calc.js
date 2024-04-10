e3f98186e6ad05827d5729e81890f0d1d1ea3b35
minto131 <158053092+minto131@users.noreply.github.com>
2024-04-10 15:43:07 +0900

    Add files via upload

---
+++ kadai_007/kadai_007/calc.js
@@ -0,0 +1,11 @@
+num = 15;
+
+//変数（num）が3と5の倍数の場合
+if (num === 3  && num === 5) {
+    console.log('3と5の倍数です');
+}
+
+//変数（num）が3と5の倍数以外の時
+else if (num === 3 && num === 5) {
+    console.log(num);
+}
